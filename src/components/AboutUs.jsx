05ee9865196cc6ab7be354ff71c2efcd0865bbbe
Salarzii619 <engarshad619.com>
2026-09-05 22:04:31 +0500

    Initial React project

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,28 @@
+function AboutUs() {
+  return (
+    <section>
+      <h1>About Us</h1>
+
+      <p>
+        Welcome to our company. We are committed to providing
+        high-quality products and services to our customers.
+      </p>
+
+      <h2>Our Mission</h2>
+
+      <p>
+        Our mission is to create innovative solutions and provide
+        excellent customer experiences.
+      </p>
+
+      <h2>Our Vision</h2>
+
+      <p>
+        We aim to become a trusted and innovative company by
+        continuously improving our products and services.
+      </p>
+    </section>
+  );
+}
+
+export default AboutUs;
